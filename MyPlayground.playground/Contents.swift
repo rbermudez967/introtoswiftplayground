178486aeb6f8c976bb30325fee9c8b718449bc4e
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-01-19 10:42:02 -1000

    Completed Swift Playgrounds #1
    Finished Swift Playgrounds 1 in class Jan 19 2021

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// declare a variable, name it, myFirstVariable and type as a string
+//var myFirstVariable: String
+
+//String is a data type represented by a string of characters, must be surrounded by ""
+
+//assign myFirstVariable to a string
+var myFirstVariable =  "I've created my first string"
+
+//String Concatenation - combining strings together
+
+//declare and assign second variable all in one line
+var mySecondVariable = "and look I combine 2 strings together"
+
+//create third variable to hold string concatenation
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+// my variables
+var  randVariable1 = "All right stop "
+var randVariable2 = "collaborate and listen "
+var randVariable3 = "Ice is back with my brand new invention."
+
+// my formula
+var randVariableCombined = randVariable1 + randVariable2 + randVariable3
+
+//crossing fingers and hoping for the best
+print (randVariableCombined)
