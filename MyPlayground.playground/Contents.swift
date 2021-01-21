ba816c9bad337cf389a4fe3bdef93d6a54c4dffc
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-01-21 10:25:21 -1000

    Commit 1.21.21 part 7
    finished part 7, results are false

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -31,3 +31,25 @@ var randVariableCombined = randVariable1 + randVariable2 + randVariable3
 
 //crossing fingers and hoping for the best
 print (randVariableCombined)
+
+//assignment 2:  introduction to true and false class 4 Thursday 1.21.21
+
+var appState = false
+
+print (appState)
+
+//declared and assigned two variables of type int
+var numberOne = 5
+var numberTwo = 7
+
+var comparison = numberOne < numberTwo
+
+print (comparison)
+
+//part 7 using the equal to operator
+var sevenUno = "alpha"
+var sevenDos = "beta"
+
+var comparisonDos = sevenDos == sevenUno
+
+print (comparisonDos)
